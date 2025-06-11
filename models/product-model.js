3adb89efd861e90e8797f0b5ea94a5d6c3c4c434
Diana <diana.dev777@gmail.com>
2025-06-11 11:13:19 +0500

    initial commit

---
+++ models/product-model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: String,
+  image: String,
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    requiered: true,
+  },
+  isFavorite: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Product = mongoose.model('Product', ProductSchema);
+module.exports = Product;
